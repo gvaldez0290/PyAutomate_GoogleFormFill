c776279a0675520f348fb3ce02a1e672664cfee2
Jerry Valdez <gvaldez0290@gmail.com>
2023-10-31 16:24:00 -0400

    update all

--- whatapp_export_automation_UW/WA_gui_automation.py
+++ whatapp_export_automation_UW/WA_gui_automation.py
@@ -1,1 +1,4 @@
 #automation of gui for getting the whats app chat export 
+# use microsoft phone companion to open whats app and select the outlook option to email the text file 
+    #Specify specific subject line
+# open the with this specific subject line in the email and download the file. 
