6f2eb5b33bb3dc031719ee61a9c04735c8b1d5cc
gvaldez0290 <gvaldez0290@gmail.com>
2023-11-05 15:31:01 -0500

    Delete whatapp_export_automation_UW directory

--- whatapp_export_automation_UW/WA_gui_automation.py
+++
@@ -1,4 +0,0 @@
-#automation of gui for getting the whats app chat export 
-# use microsoft phone companion to open whats app and select the outlook option to email the text file 
-    #Specify specific subject line
-# open the with this specific subject line in the email and download the file. 
